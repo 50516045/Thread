f74a45da8a7364f19d70194323edf2d2b5dc754d
50516045 <50516045@50516045-pc>
2017-12-22 13:54:26 +0900

    1204

---
+++ src/thread/Thread_main.java
@@ -0,0 +1,28 @@
+package thread;
+
+public class Thread_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread_run th [] = new Thread_run[3];
+		Thread_runnable th2 [] = new Thread_runnable[3];
+
+		for(int i = 0; i < 2; i++) {
+			th[i] = new Thread_run();
+			th2[i] = new Thread_runnable();
+			th[i].start();
+			th2[i].run();
+
+		    try {
+		    	th[i].join();
+		    } catch (InterruptedException e) {
+		    	// TODO Auto-generated catch block
+		    	e.printStackTrace();
+		    }
+		}
+		for(int i = 0;i<10;i++) {
+			System.out.println("main‚©‚ço—Í: "+i);
+		}	
+	}
+
+}
